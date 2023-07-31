11af879ff968d931f04cfc1b99a32181f053a1fe
AP <128137138+soft-techies@users.noreply.github.com>
2023-07-31 12:51:22 +0530

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -3,7 +3,5 @@ from time import sleep
 # the program will print hello world
 #  every 1 second foever
 while True:
-    print("[2023-07-21 02:08:43.950] [AIAGGREGATE] [DEBUG] Model: retinanet, score: -1.0, label: -1, location: -1, objid: 0, source: beta1")
-    print("AND")
-    print("[2023-07-21 02:08:43.950] [AIAGGREGATE] [DEBUG] Model: retinanet, score: -1.0, label: -1, location: -1, objid: 0, source: beta2")
+    print("Hello World")
     sleep(5)
